d5aa5ceafb6466c64faa57220c534557669b998f
Souvik Ghosh <145422565+souvikghost@users.noreply.github.com>
2023-12-14 18:00:01 +0530

    Update allURL.js

--- allURL.js
+++ allURL.js
@@ -3,4 +3,4 @@
 export const CARD_IMAGE_CDN_URL= "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_150,h_130/"
 
 
-export const MENU_CARD_DETAILS_API="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.572646&lng=88.36389500000001&restaurantId="
+export const MENU_CARD_DETAILS_API="<Swiggy's API>"
